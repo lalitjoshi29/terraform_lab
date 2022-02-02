642df90287c94c74996bff8b4dd87dc141ad9011
lalit joshi <lmjsbooks@gmail.com>
2022-02-02 16:58:22 +0530

    first terraform example

---
+++ docker_example.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "docker_lab"
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+}
